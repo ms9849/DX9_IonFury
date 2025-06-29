58ed5b0ea2c10e575298120cc3dd18dd2cf14e95
sunho-5810 <sunho5810@gmail.com>
2025-06-29 12:19:31 +0900

    AddRef, Release 추가

--- Client/Private/UIBullets.cpp
+++ Client/Private/UIBullets.cpp
@@ -53,6 +53,7 @@ HRESULT CUIBullets::Initialize(void* pArg)
 
 	m_pText = dynamic_cast<CUIText*>(m_pGameInstance->Find_GameObject_ToLayer(Desc.iLayerLevelIndex, Desc.strLayerTag));
 	m_pPlayer = dynamic_cast<CPlayer*>(m_pGameInstance->Find_GameObject_ToLayer(ENUM_CLASS(LEVEL::GAMEPLAY), TEXT("Layer_Player")));
+	Safe_AddRef(m_pPlayer);
 
 	return S_OK;
 }
@@ -157,4 +158,5 @@ void CUIBullets::Free()
 	Safe_Release(m_pTransformCom);
 	Safe_Release(m_pVIBufferCom);
 	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pPlayer);
 }
