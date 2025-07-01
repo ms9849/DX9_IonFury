5f57d879b310da283106ccd4680f2be505139920
HanSung <gkstjddlekd@gmail.com>
2025-07-02 03:34:55 +0900

    프레임 추가 수정
    - 프레임 추가로 출력하던 현상 수정
    - 몬스터 구조 변경

--- Client/Public/Monster.h
+++ Client/Public/Monster.h
@@ -41,7 +41,7 @@ protected:
 	CBoxCollider* m_pBoxColliderCom = { nullptr };
 	CSphereCollider* m_pSphereColliderCom = { nullptr };
 
-	_float m_AttackfCoolTime = 3.f;
+	_float m_fAttackfCoolTime = 3.f;
 	_float m_fSumAttackCoolTime = 3.f;
 	_float m_fMoveCoolTime = 0.05f;
 	_float m_fSumMoveCoolTime = {};
