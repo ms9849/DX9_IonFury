5e1e9a5d76ee3ec97ac18720a126bba8b4c4e945
HanSung <gkstjddlekd@gmail.com>
2025-07-17 01:04:52 +0900

    사운드 추가
    - 보스 스테이지 사운드 추가
    - 엔딩 씬 사운드 추가

--- Client/Private/Level_Ending.cpp
+++ Client/Private/Level_Ending.cpp
@@ -12,6 +12,8 @@ CLevel_Ending::CLevel_Ending(LPDIRECT3DDEVICE9 pGraphic_Device, LEVEL eLevelID)
 
 HRESULT CLevel_Ending::Initialize()
 {
+	m_pGameInstance->PlayBGM(L"corroded_192.xm", 0.7f);
+
 	if (FAILED(Ready_Layer_BackGround(TEXT("Layer_BackGround"))))
 		return E_FAIL;
 
